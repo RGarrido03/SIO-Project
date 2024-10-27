c63132cd926ea2ec17ccac2d4da31eb9c94b0f1f
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-27 17:06:13 +0000

    Fix session in crud

--- delivery1/repository/repository/crud/base.py
+++ delivery1/repository/repository/crud/base.py
@@ -1,15 +1,11 @@
-from typing import Generic, TypeVar, Type, Annotated
+from typing import Generic, TypeVar, Type
 
-from fastapi import Depends
 from sqlmodel import select, SQLModel
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 from repository.config.database import get_session
 
 ModelType = TypeVar("ModelType", bound=SQLModel)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=SQLModel)
-
-Session = Annotated[AsyncSession, Depends(get_session)]
 
 
 class CRUDBase(Generic[ModelType, CreateSchemaType]):
@@ -25,27 +21,29 @@ class CRUDBase(Generic[ModelType, CreateSchemaType]):
         """
         self.model = model
 
-    async def create(self, obj: CreateSchemaType, session: Session) -> ModelType:
+    async def create(self, obj: CreateSchemaType) -> ModelType:
+        session = await anext(get_session())
         db_obj = self.model.model_validate(obj)
         session.add(db_obj)
         await session.commit()
         await session.refresh(db_obj)
         return db_obj
 
-    async def get(self, id: int, session: Session) -> ModelType | None:
+    async def get(self, id: int) -> ModelType | None:
+        session = await anext(get_session())
         return await session.get(self.model, id)
 
-    async def get_all(self, session: Session) -> list[ModelType]:
+    async def get_all(self) -> list[ModelType]:
+        session = await anext(get_session())
         result = await session.exec(select(self.model))
         return list(result.all())
 
-    async def update(
-        self, id: int, obj: CreateSchemaType, session: Session
-    ) -> ModelType | None:
-        db_obj = await self.get(id, session)
+    async def update(self, id: int, obj: CreateSchemaType) -> ModelType | None:
+        db_obj = await self.get(id)
         if db_obj is None:
             return None
 
+        session = await anext(get_session())
         new_data = obj.model_dump(exclude_unset=True)
         db_obj.sqlmodel_update(new_data)
 
@@ -54,11 +52,12 @@ class CRUDBase(Generic[ModelType, CreateSchemaType]):
         await session.refresh(db_obj)
         return db_obj
 
-    async def delete(self, id: int, session: Session) -> bool:
-        obj = await self.get(id, session)
+    async def delete(self, id: int) -> bool:
+        obj = await self.get(id)
         if obj is None:
             return False
 
+        session = await anext(get_session())
         await session.delete(obj)
         await session.commit()
         return True
