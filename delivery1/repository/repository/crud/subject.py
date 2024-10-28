67fb7f2f8145f9c66720af337df6404282fad108
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-28 23:47:57 +0000

    Create three main CRUDs

---
+++ delivery1/repository/repository/crud/subject.py
@@ -0,0 +1,10 @@
+from repository.crud.base import CRUDBase
+from repository.models.subject import Subject, SubjectCreate
+
+
+class CRUDSubject(CRUDBase[Subject, SubjectCreate]):
+    def __init__(self) -> None:
+        super().__init__(Subject)
+
+
+crud_subject = CRUDSubject()
