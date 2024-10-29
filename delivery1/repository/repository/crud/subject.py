ad74915c935f357bc74f4e9f5aecb9e35461eaee
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-29 00:47:37 +0000

    Add initial subject endpoints, with create and activation

--- delivery1/repository/repository/crud/subject.py
+++ delivery1/repository/repository/crud/subject.py
@@ -1,3 +1,4 @@
+from repository.config.database import get_session
 from repository.crud.base import CRUDBase
 from repository.models.subject import Subject, SubjectCreate
 
@@ -5,6 +6,19 @@ from repository.models.subject import Subject, SubjectCreate
 class CRUDSubject(CRUDBase[Subject, SubjectCreate, str]):
     def __init__(self) -> None:
         super().__init__(Subject)
+
+    async def set_active(self, username: str, active: bool) -> Subject | None:
+        user = await self.get(username)
+        if user is None:
+            return None
+
+        user.active = active
+
+        session = await anext(get_session())
+        session.add(user)
+        await session.commit()
+        await session.refresh(user)
+        return user
 
 
 crud_subject = CRUDSubject()
---
+++ delivery1/repository/repository/routers/subject.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from repository.crud.subject import crud_subject
+from repository.models.subject import SubjectCreate, Subject
+
+router = APIRouter(prefix="/subject")
+
+
+@router.post("/")
+async def create_subject(subject: SubjectCreate) -> Subject:
+    return await crud_subject.create(subject)
+
+
+@router.patch("/activation")
+async def set_activation(username: str, active: bool) -> Subject | None:
+    return await crud_subject.set_active(username, active)
