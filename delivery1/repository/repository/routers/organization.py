062e1903942023037eea18092add0e692b1eea7a
André Ribeiro <andrepedro2004@hotmail.com>
2024-10-29 00:36:32 +0000

    Refactor organization urls

--- delivery1/repository/repository/routers/organization.py
+++ delivery1/repository/repository/routers/organization.py
@@ -6,11 +6,11 @@ from repository.models.organization import Organization, OrganizationCreate
 router = APIRouter(prefix="/organization")
 
 
-@router.post("/create")
+@router.post("/")
 async def create_organization(organization: OrganizationCreate) -> Organization:
     return await crud_organization.create(organization)
 
 
-@router.get("/list")
+@router.get("/")
 async def list_organizations() -> list[Organization]:
     return await crud_organization.get_all()
