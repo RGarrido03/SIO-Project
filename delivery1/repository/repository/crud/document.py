1c4c4ed563ff448782cd118ac810f3275c00a7a1
Violeta <120067589+violetar@users.noreply.github.com>
2024-10-29 00:43:51 +0000

    Add support for multiple pk types in base crud

--- delivery1/repository/repository/crud/document.py
+++ delivery1/repository/repository/crud/document.py
@@ -1,8 +1,10 @@
+import uuid
+
 from repository.crud.base import CRUDBase
 from repository.models.document import Document, DocumentCreate
 
 
-class CRUDDocument(CRUDBase[Document, DocumentCreate]):
+class CRUDDocument(CRUDBase[Document, DocumentCreate, uuid.UUID]):
     def __init__(self) -> None:
         super().__init__(Document)
 
--- delivery1/repository/repository/crud/subject.py
+++ delivery1/repository/repository/crud/subject.py
@@ -2,7 +2,7 @@ from repository.crud.base import CRUDBase
 from repository.models.subject import Subject, SubjectCreate
 
 
-class CRUDSubject(CRUDBase[Subject, SubjectCreate]):
+class CRUDSubject(CRUDBase[Subject, SubjectCreate, str]):
     def __init__(self) -> None:
         super().__init__(Subject)
 
