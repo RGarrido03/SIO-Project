bb83f8644ba2372eed3135b1f998ad64163f69e3
Rúben Garrido <57329376+rgarrido03@users.noreply.github.com>
2024-10-30 23:51:18 +0000

    Add encryption utils

---
+++ delivery1/repository/repository/utils/encryption.py
@@ -0,0 +1,25 @@
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
+from cryptography.hazmat.primitives.asymmetric.types import (
+    PublicKeyTypes,
+    PrivateKeyTypes,
+)
+
+
+def load_private_key(private_key: str, password: str | None = None) -> PrivateKeyTypes:
+    return serialization.load_pem_private_key(
+        private_key.encode(), password=password.encode() if password else None
+    )
+
+
+def load_public_key(public_key: str) -> PublicKeyTypes:
+    return serialization.load_pem_public_key(public_key.encode())
+
+
+def encrypt(data: bytes, public_key: RSAPublicKey) -> bytes:
+    return public_key.encrypt(data, padding.PKCS1v15())
+
+
+def decrypt(data: bytes, private_key: RSAPrivateKey) -> bytes:
+    return private_key.decrypt(data, padding.PKCS1v15())
