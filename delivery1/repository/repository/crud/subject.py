104f6ee66d82f2c473fee3e56c85bc612f095e4e
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-30 13:16:05 +0000

    Override create in subject crud because of public key add

--- delivery1/repository/repository/crud/subject.py
+++ delivery1/repository/repository/crud/subject.py
@@ -1,10 +1,24 @@
+from repository.config.database import get_session
 from repository.crud.base import CRUDBase
-from repository.models.subject import Subject, SubjectCreate
+from repository.crud.public_key import crud_public_key
+from repository.models.subject import Subject, SubjectCreate, PublicKeyCreate
 
 
 class CRUDSubject(CRUDBase[Subject, SubjectCreate, str]):
     def __init__(self) -> None:
         super().__init__(Subject)
+
+    async def create(self, create_obj: SubjectCreate) -> Subject:
+        subject = Subject.model_validate(create_obj)
+        subject = await self._add_to_db(subject)
+        await crud_public_key.create(
+            PublicKeyCreate(
+                key=create_obj.public_key, subject_username=subject.username
+            )
+        )
+        session = await anext(get_session())
+        await session.refresh(subject)
+        return subject
 
     async def set_active(self, username: str, active: bool) -> Subject | None:
         user = await self.get(username)
