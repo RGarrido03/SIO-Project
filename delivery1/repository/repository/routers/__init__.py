c3f69852c7f02e8d88004e6ba51c9534ad4a2d0a
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-30 01:56:05 +0000

    Add document crud and endpoints

--- delivery1/repository/repository/routers/__init__.py
+++ delivery1/repository/repository/routers/__init__.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from repository.routers import organization, subject
+from repository.routers import organization, subject, document
 
 router = APIRouter()
 router.include_router(organization.router)
 router.include_router(subject.router)
+router.include_router(document.router)
