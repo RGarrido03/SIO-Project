37ca3e911faf7384ff01ce2c7732457e281202aa
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-10-30 13:29:03 +0000

    Break SubjectOrganizationLink into parts, add crud

--- delivery1/repository/repository/models/relations.py
+++ delivery1/repository/repository/models/relations.py
@@ -3,10 +3,16 @@ import uuid
 from sqlmodel import SQLModel, Field, Relationship
 
 
-class SubjectOrganizationLink(SQLModel, table=True):
+class SubjectOrganizationLinkBase(SQLModel):
     subject_username: str = Field(foreign_key="subject.username", primary_key=True)
     organization_name: str = Field(foreign_key="organization.name", primary_key=True)
     public_key: uuid.UUID = Field(foreign_key="publickey.id")
 
+
+class SubjectOrganizationLink(SubjectOrganizationLinkBase, table=True):
     subject: "Subject" = Relationship(back_populates="organization_links")
     organization: "Organization" = Relationship(back_populates="subject_links")
+
+
+class SubjectOrganizationLinkCreate(SubjectOrganizationLinkBase):
+    pass
