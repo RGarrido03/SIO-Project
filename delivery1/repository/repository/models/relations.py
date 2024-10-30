629203459fa0d35c076c2fb5f318b41df6570bc8
Violeta <120067589+violetar@users.noreply.github.com>
2024-10-30 19:44:51 +0000

    Rename public key fk in n:m thing

--- delivery1/repository/repository/models/relations.py
+++ delivery1/repository/repository/models/relations.py
@@ -1,17 +1,20 @@
 import uuid
 
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlmodel import SQLModel, Field, Relationship
 
 
 class SubjectOrganizationLinkBase(SQLModel):
     subject_username: str = Field(foreign_key="subject.username", primary_key=True)
     organization_name: str = Field(foreign_key="organization.name", primary_key=True)
-    public_key: uuid.UUID = Field(foreign_key="publickey.id")
+    public_key_id: uuid.UUID = Field(foreign_key="publickey.id")
 
 
 class SubjectOrganizationLink(SubjectOrganizationLinkBase, table=True):
     subject: "Subject" = Relationship(back_populates="organization_links")
     organization: "Organization" = Relationship(back_populates="subject_links")
+    publickey: "PublicKey" = Relationship()
 
 
 class SubjectOrganizationLinkCreate(SubjectOrganizationLinkBase):
