d22e3b51932b2eeea1502b0280ef0919d47a7db9
Violeta <120067589+violetar@users.noreply.github.com>
2024-10-31 00:35:06 +0000

    Create session (still some changes to do)

--- delivery1/repository/repository/routers/subject.py
+++ delivery1/repository/repository/routers/subject.py
@@ -1,6 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from repository.crud.subject import crud_subject
+from repository.crud.subject_organization_link import crud_subject_organization_link
+from repository.models.session import SessionCreate, Session
 from repository.models.subject import SubjectCreate, Subject
 
 router = APIRouter(prefix="/subject", tags=["Subject"])
@@ -10,6 +12,14 @@ router = APIRouter(prefix="/subject", tags=["Subject"])
 async def create_subject(subject: SubjectCreate) -> Subject:
     obj = await crud_subject.create(subject)
     return obj.subject
+
+
+@router.post("/session")
+async def create_session(info: SessionCreate) -> Session:
+    try:
+        return await crud_subject_organization_link.create_session(info)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.patch("/activation")
