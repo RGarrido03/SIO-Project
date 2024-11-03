ba04f103d09e08bab8819f9b3fa8a43fe0185205
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-03 22:31:19 +0000

    Decrypt request middleware (not tested, not refactored)

---
+++ delivery1/repository/repository/utils/middleware.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+import jwt
+from fastapi import Request
+
+from repository.config.settings import settings
+from repository.utils.encryption.encryptors import decrypt_asymmetric, decrypt_symmetric
+
+
+async def decrypt_request(request: Request) -> Request:
+    data = await request.body()
+    if not data:
+        return request
+
+    match request.headers.get("Encryption"):
+        case "repository":
+            data = decrypt_asymmetric(data, settings.KEYS[0])
+        case "session":
+            auth_header = request.headers.get("Authorization")
+            if auth_header is None:
+                return request
+
+            token = decrypt_asymmetric(auth_header.encode(), settings.KEYS[0])
+
+            # Update headers with decrypted token
+            headers = dict(request.scope["headers"])
+            headers[b"Authorization"] = token
+            request.scope["headers"] = [(k, v) for k, v in headers.items()]
+
+            payload: dict[str, Any] = jwt.decode(
+                token.decode(),
+                settings.AUTH_SECRET_KEY,
+                algorithms=[settings.AUTH_ALGORITHM],
+            )
+            key: str = payload.get("keys", [])[0]
+            data = decrypt_symmetric(data, key.encode())
+        case _:
+            return request
+
+    request._body = data
+    return request
