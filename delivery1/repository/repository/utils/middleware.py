02aaeee6bd35625b95f36e1f03f101cfdf887eab
Rúben Garrido <57329376+rgarrido03@users.noreply.github.com>
2024-11-04 20:01:58 +0000

    [Repository] Return request upon token decrypt

--- delivery1/repository/repository/utils/middleware.py
+++ delivery1/repository/repository/utils/middleware.py
@@ -8,12 +8,12 @@ from repository.config.settings import settings
 from repository.utils.encryption.encryptors import decrypt_asymmetric, decrypt_symmetric
 
 
-async def decrypt_request_key(request: Request) -> bytes | None:
+async def decrypt_request_key(request: Request) -> tuple[Request, bytes | None]:
     if (encryption := request.headers.get("Encryption")) is None:
-        return None
+        return request, None
 
     if (auth_header := request.headers.get("Authorization")) is None:
-        return None
+        return request, None
 
     auth_header_bytes = (
         auth_header.encode().replace(b"\\n", b"\n").replace(b"\\r", b"\r")
@@ -21,18 +21,18 @@ async def decrypt_request_key(request: Request) -> bytes | None:
     token = decrypt_asymmetric(base64.decodebytes(auth_header_bytes), settings.KEYS[0])
 
     headers = dict(request.scope["headers"])
-    headers[b"Authorization"] = token
+    headers[b"authorization"] = token
     request.scope["headers"] = [(k, v) for k, v in headers.items()]
 
     if encryption != "session":
-        return token
+        return request, token
 
     payload: dict[str, Any] = jwt.decode(
         token,
         settings.AUTH_SECRET_KEY,
         algorithms=[settings.AUTH_ALGORITHM],
     )
-    return payload.get("keys", [])[0].encode()
+    return request, payload.get("keys", [])[0].encode()
 
 
 async def decrypt_request_body(request: Request, token: bytes | None) -> None:
