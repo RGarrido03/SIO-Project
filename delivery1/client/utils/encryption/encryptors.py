e3a40dacd75ebbde56e806dcd5ae75edddb1f86e
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-04 02:02:11 +0000

    Fix circular imports

--- delivery1/client/utils/encryption/encryptors.py
+++ delivery1/client/utils/encryption/encryptors.py
@@ -2,30 +2,11 @@ import base64
 import json
 from typing import Any
 
-from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey, RSAPublicKey
-from cryptography.hazmat.primitives.asymmetric.types import (
-    PublicKeyTypes,
-)
 from cryptography.hazmat.primitives.ciphers import algorithms, modes, Cipher
 
 from utils.repository_info import get_repository_public_key
-
-
-def load_private_key(
-    private_key: bytes, password: str | None = None
-) -> tuple[RSAPrivateKey, RSAPublicKey]:
-    pk = serialization.load_ssh_private_key(
-        private_key, password=password.encode() if password else None
-    )
-    if not isinstance(pk, RSAPrivateKey):
-        raise ValueError("Invalid private key")
-    return pk, pk.public_key()
-
-
-def load_public_key(public_key: str) -> PublicKeyTypes:
-    return serialization.load_pem_public_key(public_key.encode())
 
 
 def encrypt_symmetric(data: bytes, key: bytes, iv: bytes) -> bytes:
