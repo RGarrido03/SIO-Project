b86c1639a726cc2e1be7bec4f52ac08e386e724b
Rúben Garrido <57329376+rgarrido03@users.noreply.github.com>
2024-11-04 02:02:55 +0000

    Fix public key fetching and storing

--- delivery1/client/utils/repository_info.py
+++ delivery1/client/utils/repository_info.py
@@ -1,10 +1,9 @@
 from pathlib import Path
 
 import requests
-import typer
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 
-from utils.encryption import load_public_key
+from utils.encryption.loaders import load_public_key
 
 
 def _fetch_repository_public_key() -> str:
@@ -13,18 +12,13 @@ def _fetch_repository_public_key() -> str:
 
 
 def get_repository_public_key() -> RSAPublicKey:
-    file = (
-        Path(typer.get_app_dir("SIO Project - Client"))
-        / "storage"
-        / "repository"
-        / "public_key.pem"
-    )
+    file = Path(__file__).parent.parent / "storage" / "repository" / "public_key.pem"
 
     if file.exists():
         with open(file, "r") as f:
             return load_public_key(f.read())
 
-    public = _fetch_repository_public_key()
-    with open(file, "w") as f:
+    public = "\n".join(_fetch_repository_public_key().strip('"').split("\\n"))
+    with open(file, "w+") as f:
         f.write(public)
     return load_public_key(public)
