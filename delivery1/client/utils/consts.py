0f41eac30338f6eee9baf5301881b7473b72b868
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-04 18:44:23 +0000

    [Client] Use constants

---
+++ delivery1/client/utils/consts.py
@@ -0,0 +1,6 @@
+BASE_URL = "http://localhost:8000"
+
+DOCUMENT_URL = f"{BASE_URL}/document"
+ORGANIZATION_URL = f"{BASE_URL}/organization"
+REPOSITORY_URL = f"{BASE_URL}/repository"
+SUBJECT_URL = f"{BASE_URL}/subject"
