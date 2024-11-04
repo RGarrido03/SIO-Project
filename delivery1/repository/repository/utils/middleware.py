ff252b18f9dd9dbcbbf8bdde9e833fd8723d0d01
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-04 15:34:38 +0000

    Repository: Refactor encryption to use the Authorization header
    Co-authored-by: Violeta Ramos <VioletaR@users.noreply.github.com>

--- delivery1/repository/repository/utils/middleware.py
+++ delivery1/repository/repository/utils/middleware.py
@@ -8,42 +8,47 @@ from repository.config.settings import settings
 from repository.utils.encryption.encryptors import decrypt_asymmetric, decrypt_symmetric
 
 
-async def decrypt_request_token(request: Request) -> Request:
-    if request.headers.get("Encryption") != "session":
+async def decrypt_request_key(request: Request) -> bytes | None:
+    if (encryption := request.headers.get("Encryption")) is None:
+        return None
+
+    if (auth_header := request.headers.get("Authorization")) is None:
+        return None
+
+    match encryption:
+        case "repository":
+            auth_header_bytes = (
+                auth_header.encode().replace(b"\\n", b"\n").replace(b"\\r", b"\r")
+            )
+            return decrypt_asymmetric(
+                base64.decodebytes(auth_header_bytes), settings.KEYS[0]
+            )
+        case "session":
+            payload: dict[str, Any] = jwt.decode(
+                auth_header,
+                settings.AUTH_SECRET_KEY,
+                algorithms=[settings.AUTH_ALGORITHM],
+            )
+            return payload.get("keys", [])[0].encode()
+        case _:
+            return None
+
+
+async def decrypt_request_body(request: Request, token: bytes | None) -> Request:
+    if token is None:
         return request
 
-    auth_header = request.headers.get("Authorization")
-    if auth_header is None:
-        return request
-
-    auth_bytes = base64.decodebytes(auth_header.encode())
-    token = decrypt_asymmetric(auth_bytes, settings.KEYS[0])
-
-    headers = dict(request.scope["headers"])
-    headers[b"Authorization"] = token
-    request.scope["headers"] = [(k, v) for k, v in headers.items()]
-
-    return request
-
-
-async def decrypt_request_body(request: Request) -> Request:
     data = await request.body()
-    data = base64.decodebytes(data)
     if not data:
         return request
 
-    print("pixola", data)
-
     match request.headers.get("Encryption"):
         case "repository":
-            data = decrypt_asymmetric(data, settings.KEYS[0])
+            data = base64.decodebytes(data)
+            data = decrypt_symmetric(data, token)
         case "session":
-            auth_header = request.headers.get("Authorization")
-            if auth_header is None:
-                return request
-
             payload: dict[str, Any] = jwt.decode(
-                auth_header,
+                token.decode(),
                 settings.AUTH_SECRET_KEY,
                 algorithms=[settings.AUTH_ALGORITHM],
             )
@@ -52,6 +57,5 @@ async def decrypt_request_body(request: Request) -> Request:
         case _:
             return request
 
-    print("pixa", data)
     request._body = data
     return request
