6f194cb9d026f752735d1c777af64d9813b06e6f
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-04 15:43:29 +0000

    Repository: Refactor token decrypt
    Co-authored-by: Violeta Ramos <VioletaR@users.noreply.github.com>

--- delivery1/repository/repository/utils/middleware.py
+++ delivery1/repository/repository/utils/middleware.py
@@ -15,23 +15,20 @@ async def decrypt_request_key(request: Request) -> bytes | None:
     if (auth_header := request.headers.get("Authorization")) is None:
         return None
 
-    match encryption:
-        case "repository":
-            auth_header_bytes = (
-                auth_header.encode().replace(b"\\n", b"\n").replace(b"\\r", b"\r")
-            )
-            return decrypt_asymmetric(
-                base64.decodebytes(auth_header_bytes), settings.KEYS[0]
-            )
-        case "session":
-            payload: dict[str, Any] = jwt.decode(
-                auth_header,
-                settings.AUTH_SECRET_KEY,
-                algorithms=[settings.AUTH_ALGORITHM],
-            )
-            return payload.get("keys", [])[0].encode()
-        case _:
-            return None
+    auth_header_bytes = (
+        auth_header.encode().replace(b"\\n", b"\n").replace(b"\\r", b"\r")
+    )
+    token = decrypt_asymmetric(base64.decodebytes(auth_header_bytes), settings.KEYS[0])
+
+    if encryption != "session":
+        return token
+
+    payload: dict[str, Any] = jwt.decode(
+        token,
+        settings.AUTH_SECRET_KEY,
+        algorithms=[settings.AUTH_ALGORITHM],
+    )
+    return payload.get("keys", [])[0].encode()
 
 
 async def decrypt_request_body(request: Request, token: bytes | None) -> Request:
