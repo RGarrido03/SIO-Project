9efa0fd7d654cc8192679d28d237fb040ff63ffa
Violeta <120067589+violetar@users.noreply.github.com>
2024-11-04 19:55:25 +0000

    [Client] Add list documents command

---
+++ delivery1/client/commands/authenticated.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+from typing import Annotated
+
+import requests
+import typer
+
+from utils.consts import DOCUMENT_URL
+from utils.encryption.encryptors import encrypt_asymmetric
+from utils.repository_info import get_repository_public_key
+
+app = typer.Typer()
+
+
+@app.command("rep_list_docs")
+def list_documents(
+    session_file: Path,
+    username: Annotated[
+        str | None,
+        typer.Option("-s", "--username", help="Filter by username"),
+    ] = None,
+    date: Annotated[
+        tuple[str, str] | None,
+        typer.Option("-d", "--date", help="Filter by date"),
+    ] = None,
+):
+    if date is not None and date[0] not in ["nt", "ot", "et"]:
+        raise typer.BadParameter(
+            "Invalid date filter. Must be one of 'nt', 'ot', 'et'."
+        )
+
+    key = encrypt_asymmetric(session_file.read_bytes(), get_repository_public_key())
+
+    params = {
+        "username": username,
+        "date": date[1] if date is not None else None,
+        "date_order": date[0] if date is not None else None,
+    }
+
+    response = requests.get(
+        DOCUMENT_URL,
+        params=params,
+        headers={
+            "Encryption": "session",
+            "Authorization": key,
+        },
+    )
+    body = response.json()
+    print(body)
