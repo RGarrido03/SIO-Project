86575abf61db2d946ff5488cdb2e75e08dff05ca
André Ribeiro <andrepedro2004@hotmail.com>
2024-11-04 12:15:46 +0000

    Repo: Initialization vector endpoint

--- delivery1/repository/repository/routers/repository.py
+++ delivery1/repository/repository/routers/repository.py
@@ -1,3 +1,5 @@
+import base64
+
 from cryptography.hazmat.primitives._serialization import Encoding, PublicFormat
 from fastapi import APIRouter
 
@@ -6,6 +8,11 @@ from repository.config.settings import settings
 router = APIRouter(prefix="/repository", tags=["Repository"])
 
 
-@router.get("/public")
+@router.get("/public_key")
 async def get_public_key() -> str:
     return settings.KEYS[1].public_bytes(Encoding.PEM, PublicFormat.PKCS1).decode()
+
+
+@router.get("/iv")
+async def get_iv() -> str:
+    return base64.encodebytes(settings.INITIALIZATION_VECTOR).decode()
