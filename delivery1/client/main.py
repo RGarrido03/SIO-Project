5aef95a5cec75967bef79fbc10b55429e4aed116
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-04 19:55:25 +0000

    [Client] Add list documents command

--- delivery1/client/main.py
+++ delivery1/client/main.py
@@ -1,10 +1,10 @@
 import typer
 
-from commands import anonymous
+from commands import anonymous, authenticated
 
 app = typer.Typer(name="SIO Project - Client")
 app.registered_commands.extend(anonymous.app.registered_commands)
-
+app.registered_commands.extend(authenticated.app.registered_commands)
 
 if __name__ == "__main__":
     app()
