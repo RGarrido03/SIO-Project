992c832d03fe560a0bdea0c39b02374621f34b66
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-11 10:32:45 +0000

    Change to pem private key in load private

--- delivery1/client/utils/encryption/loaders.py
+++ delivery1/client/utils/encryption/loaders.py
@@ -8,7 +8,7 @@ from cryptography.hazmat.primitives.asymmetric.types import (
 def load_private_key(
     private_key: bytes, password: str | None = None
 ) -> tuple[RSAPrivateKey, RSAPublicKey]:
-    pk = serialization.load_ssh_private_key(
+    pk = serialization.load_pem_private_key(
         private_key, password=password.encode() if password else None
     )
     if not isinstance(pk, RSAPrivateKey):
