0c95b9340a0af5f178b00ec65327abe06da2bcfa
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-12 13:43:38 +0000

    Add jwt validation functions

--- delivery1/repository/repository/models/permission.py
+++ delivery1/repository/repository/models/permission.py
@@ -1,19 +1,23 @@
 from enum import Enum
 
 
-class RoleEnum(str,Enum):
+class RoleEnum(str, Enum):
     # TODO: Add more
     MANAGERS = "managers"
     USER = "user"
 
 
-class DocumentPermission(str,Enum):
+class Permission(str, Enum):
+    pass
+
+
+class DocumentPermission(Permission):
     DOC_ACL = "doc_acl"
     DOC_READ = "doc_read"
     DOC_DELETE = "doc_delete"
 
 
-class OrganizationPermission(Enum):
+class OrganizationPermission(Permission):
     ROLE_ACL = "role_acl"
     SUBJECT_NEW = "subject_new"
     SUBJECT_DOWN = "subject_down"
@@ -21,8 +25,35 @@ class OrganizationPermission(Enum):
     DOC_NEW = "doc_new"
 
 
-class RolePermission(Enum):
+class RolePermission(Permission):
     ROLE_NEW = "role_new"
     ROLE_DOWN = "role_down"
     ROLE_UP = "role_up"
     ROLE_MOD = "role_mod"
+
+
+# TODO: Set permissions map, this is just a placeholder (delivery 2)
+permissions_map: dict[RoleEnum, list[Permission]] = {
+    RoleEnum.MANAGERS: [
+        DocumentPermission.DOC_ACL,
+        DocumentPermission.DOC_READ,
+        DocumentPermission.DOC_DELETE,
+        OrganizationPermission.ROLE_ACL,
+        OrganizationPermission.SUBJECT_NEW,
+        OrganizationPermission.SUBJECT_DOWN,
+        OrganizationPermission.SUBJECT_UP,
+        OrganizationPermission.DOC_NEW,
+        RolePermission.ROLE_NEW,
+        RolePermission.ROLE_DOWN,
+        RolePermission.ROLE_UP,
+        RolePermission.ROLE_MOD,
+    ],
+    RoleEnum.USER: [
+        DocumentPermission.DOC_READ,
+        OrganizationPermission.ROLE_ACL,
+        OrganizationPermission.SUBJECT_NEW,
+        OrganizationPermission.SUBJECT_DOWN,
+        OrganizationPermission.SUBJECT_UP,
+        OrganizationPermission.DOC_NEW,
+    ],
+}
