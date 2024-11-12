5b12d19a240318a980e652f5a6048d76a74c992a
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-11-12 13:43:38 +0000

    Add jwt validation functions

--- delivery1/repository/repository/utils/auth/authorization_handler.py
+++ delivery1/repository/repository/utils/auth/authorization_handler.py
@@ -1,16 +1,24 @@
 from datetime import datetime
-from typing import Annotated
+from typing import Annotated, cast
 
 import jwt
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
 from jwt import InvalidTokenError
-from starlette import status
 
 from repository.config.settings import settings
 from repository.crud.subject import crud_subject
 from repository.crud.subject_organization_link import crud_subject_organization_link
+from repository.models.permission import permissions_map
 from repository.models.relations import SubjectOrganizationLink
+from repository.models.session import Session
+from repository.utils.exceptions import (
+    credentials_exception,
+    inactive_user_exception,
+    session_expired_exception,
+    no_session_exception,
+    not_enough_permissions_exception,
+)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/subject/session")
 
@@ -18,11 +26,6 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/subject/session")
 async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)]
 ) -> SubjectOrganizationLink:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
         payload = jwt.decode(
             token, settings.AUTH_SECRET_KEY, algorithms=[settings.AUTH_ALGORITHM]
@@ -36,13 +39,32 @@ async def get_current_user(
     if subject is None:
         raise credentials_exception
     if not subject.active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise inactive_user_exception
 
     link = await crud_subject_organization_link.get((username, organization))
     if link is None:
         raise credentials_exception
     if link.session is None:
-        raise credentials_exception
+        raise no_session_exception
     if link.session.expires < datetime.now():
-        raise HTTPException(status_code=400, detail="Session expired")
+        raise session_expired_exception
+    return link
+
+
+def check_permission(
+    security_scopes: SecurityScopes,
+    link: Annotated[SubjectOrganizationLink, Depends(get_current_user)],
+) -> SubjectOrganizationLink:
+    session = cast(Session, link.session)  # Session is never None in this context
+
+    permissions_in_session = {
+        perm for role in session.roles for perm in permissions_map.get(role, [])
+    }
+
+    if any(
+        permission not in permissions_in_session
+        for permission in security_scopes.scopes
+    ):
+        raise not_enough_permissions_exception
+
     return link
