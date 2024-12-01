c1de0f1ddd85219fa03ace039e2161e36206f99c
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-12-01 15:41:15 +0000

    Add role router

--- delivery2/repository/repository/routers/__init__.py
+++ delivery2/repository/repository/routers/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from repository.routers import organization, subject, document, repository
+from repository.routers import organization, subject, document, repository, role
 
 router = APIRouter()
 router.include_router(organization.router)
 router.include_router(subject.router)
 router.include_router(document.router)
 router.include_router(repository.router)
+router.include_router(role.router)
