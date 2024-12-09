8117eee235db6886ef09b7f696dcad39d5984edf
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-12-09 20:22:05 +0000

    List roles by permission and permissions by role
    Co-authored-by: André Ribeiro <andrepedro2004@hotmail.com>
    Co-authored-by: Violeta <120067589+violetar@users.noreply.github.com>

--- delivery2/repository/repository/crud/organization_role.py
+++ delivery2/repository/repository/crud/organization_role.py
@@ -1,6 +1,10 @@
+from sqlmodel import select
+
+from repository.config.database import get_session
 from repository.crud.base import CRUDBase
 from repository.models import OrganizationRole
 from repository.models.organization import OrganizationRoleBase
+from repository.models.permission import Permission
 
 
 class CRUDOrganizationRole(
@@ -8,6 +12,18 @@ class CRUDOrganizationRole(
 ):
     def __init__(self) -> None:
         super().__init__(OrganizationRole)
+
+    async def get_roles_by_permission(
+        self, organization_name: str, permission: Permission
+    ) -> list[str]:
+        async with get_session() as session:
+            result = await session.exec(
+                select(OrganizationRole)
+                .where(OrganizationRole.organization_name == organization_name)
+                .where(OrganizationRole.permissions.contains({permission}))
+            )
+            data: list[OrganizationRole] = list(result.all())
+            return [role.role for role in data]
 
 
 crud_organization_role = CRUDOrganizationRole()
--- delivery2/repository/repository/routers/role.py
+++ delivery2/repository/repository/routers/role.py
@@ -1,3 +1,30 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+
+from repository.crud.organization_role import crud_organization_role
+from repository.models import SubjectOrganizationLink
+from repository.models.permission import Permission
+from repository.utils.auth.authorization_handler import get_current_user
 
 router = APIRouter(prefix="/role", tags=["Role"])
+
+
+@router.get("/permission", description="rep_list_role_permissions")
+async def list_role_permissions(
+    role: str, link: Annotated[SubjectOrganizationLink, Depends(get_current_user)]
+) -> list[str]:
+    role_obj = await crud_organization_role.get((link.organization_name, role))
+    if role_obj is None:
+        raise HTTPException(status_code=404, detail="Role not found")
+    return role_obj.permissions
+
+
+@router.get("", description="rep_list_permission_roles")
+async def list_roles_by_permission(
+    permission: Permission,
+    link: Annotated[SubjectOrganizationLink, Depends(get_current_user)],
+) -> list[str]:
+    return await crud_organization_role.get_roles_by_permission(
+        link.organization_name, permission
+    )
