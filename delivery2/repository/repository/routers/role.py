36eef7b456f3a27dfcce9529f0b90f6357b04491
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2024-12-10 01:26:42 +0000

    Hmm why a PATCH?
    Co-authored-by: André Ribeiro <andrepedro2004@hotmail.com>
    Co-authored-by: Violeta <120067589+violetar@users.noreply.github.com>

--- delivery2/repository/repository/routers/role.py
+++ delivery2/repository/repository/routers/role.py
@@ -1,4 +1,4 @@
-from typing import Annotated, Literal
+from typing import Annotated
 
 from fastapi import APIRouter, Depends, HTTPException
 
@@ -27,14 +27,31 @@ async def add_role(
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.get("/permission", description="rep_list_role_permissions")
-async def list_role_permissions(
-    role: str, link: Annotated[SubjectOrganizationLink, Depends(get_current_user)]
-) -> list[str]:
-    role_obj = await crud_organization_role.get((link.organization_name, role))
-    if role_obj is None:
-        raise HTTPException(status_code=404, detail="Role not found")
-    return role_obj.permissions
+@router.post("/activation", description="rep_reactivate_role")
+async def activate_role(
+    role: str,
+    link: Annotated[SubjectOrganizationLink, Depends(get_current_user)],
+) -> OrganizationRole:
+    try:
+        return await crud_organization_role.set_activation(
+            link.organization_name, role, True
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+@router.post("/permission", description="rep_add_permission")
+async def add_permission_to_role(
+    role: str,
+    permission: Permission,
+    link: Annotated[SubjectOrganizationLink, Depends(get_current_user)],
+) -> OrganizationRole:
+    try:
+        return await crud_organization_role.set_permission(
+            link.organization_name, role, permission, "add"
+        )
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 @router.get("", description="rep_list_permission_roles")
@@ -47,30 +64,38 @@ async def list_roles_by_permission(
     )
 
 
-@router.patch("/activation", description="rep_suspend_role, rep_reactivate_role")
-async def toggle_role_activation(
+@router.get("/permission", description="rep_list_role_permissions")
+async def list_role_permissions(
+    role: str, link: Annotated[SubjectOrganizationLink, Depends(get_current_user)]
+) -> list[str]:
+    role_obj = await crud_organization_role.get((link.organization_name, role))
+    if role_obj is None:
+        raise HTTPException(status_code=404, detail="Role not found")
+    return role_obj.permissions
+
+
+@router.delete("/activation", description="rep_suspend_role")
+async def suspend_role(
     role: str,
-    active: bool,
     link: Annotated[SubjectOrganizationLink, Depends(get_current_user)],
 ) -> OrganizationRole:
     try:
         return await crud_organization_role.set_activation(
-            link.organization_name, role, active
+            link.organization_name, role, False
         )
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 
-@router.patch("/permission", description="rep_add_permission, rep_remove_permission")
-async def toggle_role_permission(
+@router.delete("/permission", description="rep_remove_permission")
+async def remove_permission_from_role(
     role: str,
     permission: Permission,
-    change: Literal["add", "remove"],
     link: Annotated[SubjectOrganizationLink, Depends(get_current_user)],
 ) -> OrganizationRole:
     try:
         return await crud_organization_role.set_permission(
-            link.organization_name, role, permission, change
+            link.organization_name, role, permission, "remove"
         )
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
